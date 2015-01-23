f4fdb14de6002179d183386108bf8f08ee394a84
Benedikt Kolb <benediktkolb86@gmail.com>
2015-01-23 15:41:03 +0000

    get first code up!

--- hpfilter_bk.m
+++ hpfilter_bk.m
@@ -1,43 +1,46 @@
 
-
-%   [s] = hpfilter(y,w,'makeplot')
-%   'makeplot' in the input, plots the graphics of the original series
-%   against the filtered series, if more than one series is being
-%   considered the program will plot all of them in different axes
-
-function fltd = hpfilter_bk(y,w,graph)
+function [cyc,tr] = hpfilter_bk(y,w,graph)
 
 % Applies the Hodrick-Prescott filter.
-% Usage:   fltd = hpfilter_bk(y,w,graph)
-% Inputs:  y:     original series
-%          w:     smoothing parameter (choose 100 for annual, 1600 for 
-%                 quarterly and 14400 for monthly data)
-%          graph: logical; if 1, filtered series will be plotted    
-% Outputs: fltd: filtered series
-% by B.Kolb (21/01/14), based on code by Wilmer Henao. For a derivation of the formulas used, see e.g. http://economics.sas.upenn.edu/~jesusfv/lecturetechnical5_spectral.pdf
-
+% Usage:   [cyc,tr] = hpfilter_bk(y,w,graph)
+% Inputs:  y:     Original series
+%          w:     Smoothing parameter; default is 1600 (choose 100 for
+%                 annual, 1600 for quarterly and 14400 for monthly data)
+%          graph: If graph=1, the results will be plotted (optional)
+% Outputs: cyc: Cyclical component of filtered series
+%          tr:  Trend component of filtered series
+% by Benedikt Kolb (21/01/14), based on code by Wilmer Henao
+%
+% For a derivation of the formulas used, see e.g.
+% http://economics.sas.upenn.edu/~jesusfv/lecturetechnical5_spectral.pdf
 
 if nargin == 1
-    warning('hp:no_w','No smoothing parameter w specified. I assume you want to use w = 1600 in the following.');
+    warning('hp:no_w','No smoothing parameter w specified. I assume you want to use w=1600 in the following.');
+    w = 1600;
 end
 
 if size(y,1) < size(y,2)
-    y = y';     
+    y = y';
 end
 [t n] = size(y);
 
-d = repmat([w -4*w ((6*w+1)/2)], t, 1);
+d = repmat([w -4*w (6*w+1)/2], t, 1);
 d(1,2) = -2*w;      d(t-1,2) = -2*w;
 d(1,3) = (1+w)/2;   d(t,3) = (1+w)/2;
 d(2,3) = (5*w+1)/2; d(t-1,3) = (5*w+1)/2;
-B = spdiags(d, -2:0, t, t);    %I use a sparse version of B, because when m is large, B will have many zeros     
+B = spdiags(d, -2:0, t, t);    % sparse version of B (due to W. Henao)
 B = B+B';
-fltd = B\y;
+tr  = B\y;
+cyc = y - tr;
 
-if nargin(3) == 1
-    for i = 1:t
-        figure(i)
-        subplot(floor(n/3),ceil(n/floor(n/3)),i);
-        plot(fltd(:,i),'r');   grid on;   hold on;   plot(y(:,i));   title(['Series #',num2str(i)]);
-    end
+% plot
+if nargin == 3 && graph == 1
+    figure('Name','HP-detrended series')
+        for i = 1:n
+            subplot(ceil(n/floor((n+2)/3)),floor((n+2)/3),i);
+            plot(1:t,cyc(:,i)); grid on; title(['Series #',num2str(i)]);
+            if i==1, legend('Cyclical component','Location','Northwest'), end
+        end
+else
+    return
 end
